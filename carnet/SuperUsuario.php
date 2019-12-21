c44327422f13aef71dc5afd9217ec858f024bbe7
Matthew Almeida <55630599+Lamadelrae@users.noreply.github.com>
2019-12-21 11:35:22 -0300

    Code Improvements

--- carnet/SuperUsuario.php
+++ carnet/SuperUsuario.php
@@ -6,13 +6,6 @@
   <link rel="stylesheet" href="css/style_nav.css">
   <link rel="stylesheet" href="css/style_table.css">
   <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  <script src="js/jquery-1.2.6.pack.js" type="text/javascript"></script>
-  <script src="js/jquery.maskedinput-1.1.4.pack.js" type="text/javascript" /></script>
-
   <meta charset='utf-8'>
   <meta name="viewport" content="width=device-width, initial-scale=1">
    <?php   $db = mysqli_connect("localhost", "root", "", "carnet"); 
@@ -46,11 +39,11 @@
     </ul>
   </div>
 
-     <div class="left">
+<div class="left">
      <span class="navbar-text">
       <a href="#" style="color:white"> Usuário Ativo: <b>ROOT</b></a>
     </span>
-  </div>
+</div>
 </nav>
 
 
@@ -73,14 +66,14 @@
     <?php while ($row = $result->fetch_assoc()): ?>
     <tbody>
       <tr>
-      <td><?php echo $row['cnpj']; ?></td>
-      <td><?php echo $row['valor_total_parcelas']; ?></td>
-      <td><?php echo $row['parcelas']; ?></td>
-      <td><?php echo $row['password']; ?></td>
-      <td><?php echo $row['numero_contrato']; ?></td>
-      <td><a class="btn btn-danger" target="_blank" href="cadastrocliente.php?cadastro=<?php echo $row['cnpj']; ?>">Cadastro</a></td>
+        <td><?php echo $row['cnpj']; ?></td>
+        <td><?php echo $row['valor_total_parcelas']; ?></td>
+        <td><?php echo $row['parcelas']; ?></td>
+        <td><?php echo $row['password']; ?></td>
+        <td><?php echo $row['numero_contrato']; ?></td>
+        <td><a class="btn btn-danger" target="_blank" href="cadastrocliente.php?cadastro=<?php echo $row['cnpj']; ?>">Cadastro</a></td>
       </tr>
-      <?php endwhile; ?>
+     <?php endwhile; ?>
     </tbody>
   </table>
 </div>
@@ -91,6 +84,7 @@
 
 
 ?>
+
 <div style="background-color:#DCDCDC;" class="container">
   <h2>Investidores</h2>         
   <table class="table table-borderless">
@@ -101,14 +95,15 @@
         <th></th>
       </tr>
     </thead>
+
     <?php while ($row2 = $result2->fetch_assoc()): ?>
     <tbody>
       <tr>
-      <td><?php echo $row2['cpf']; ?></td>
-      <td><?php echo $row2['password']; ?></td>
-      <td><a class="btn btn-danger" target="_blank" href="contratos_investidor.php?cpf=<?php echo $row2['cpf']; ?>">Contratos</a></td>
+         <td><?php echo $row2['cpf']; ?></td>
+         <td><?php echo $row2['password']; ?></td>
+         <td><a class="btn btn-danger" target="_blank" href="contratos_investidor.php?cpf=<?php echo $row2['cpf']; ?>">Contratos</a></td>
       </tr>
-      <?php endwhile; ?>
+    <?php endwhile; ?>
     </tbody>
   </table>
 </div>
@@ -116,9 +111,12 @@
 
 
 </body>
-
-
-
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="js/jquery-1.2.6.pack.js" type="text/javascript"></script>
+  <script src="js/jquery.maskedinput-1.1.4.pack.js" type="text/javascript" /></script>
 </html>
 
 
